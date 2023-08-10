5e2d3559459e2e970905a5054f1ac4b72c3e6627
NicolasLucero <nicolucerocipo10@gmail.com>
2023-08-10 16:31:12 -0300

    Vista ticket

--- src/Components/Presentation/AdminNav/AdminNav.tsx
+++ src/Components/Presentation/AdminNav/AdminNav.tsx
@@ -1,23 +1,27 @@
 import Link from "next/link";
 import styles from "./AdminNav.module.css";
-import { FaFilter, FaUpload, FaRegFile, FaUsers } from "react-icons/fa";
+import {
+  svgCrown,
+  svgHeart,
+  svgMessage,
+  svgNavCub,
+  svgOut,
+  svgBack,
+} from "../Icons/icons";
 
 const AdminNav: React.FC = () => {
   return (
     <aside className={styles.adminNav}>
+      <span className={styles.back}>{svgBack()}</span>
+      <span className={styles.navTop}>
+        <Link href="/admin/categories">{svgNavCub()}</Link>
+        <Link href="/admin/upload">{svgHeart()}</Link>
+        <Link href="/">{svgCrown()}</Link>
+        <Link href="/">{svgMessage()}</Link>
+      </span>
+
       <span>
-        <Link href="/admin/categories">
-          <FaFilter />
-        </Link>
-        <Link href="/admin/upload">
-          <FaUpload />
-        </Link>
-        <Link href="/">
-          <FaRegFile />
-        </Link>
-        <Link href="/">
-          <FaUsers />
-        </Link>
+        <Link href="">{svgOut()}</Link>
       </span>
     </aside>
   );
