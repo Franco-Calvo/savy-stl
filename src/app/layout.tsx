6426443499dcc14af859a710c22a60ab2f68ecff
Franco Calvo <fcalvo33@gmail.com>
2023-08-28 19:21:48 -0300

    production fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Navbar from "@/Components/Presentation/Navbar/Navbar";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import { ReduxProvider } from "@/store/provider";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Savy Stl",
-  description: "Changing 3D print",
-};
 
 export default function RootLayout({
   children,
